1dc584de0dec3730122651e9207435270a0b966f
OmPrakash Rathore <omr31997@gmail.com>
2024-09-13 20:24:04 +0000

    type_conversion example

---
+++ 01_Basics/02_conversionOperation.js
@@ -0,0 +1,63 @@
+// let score_0=33;
+// let score_0x="33";
+// let score_1="33abc";
+// let score_2=true;
+// let score_3=false;
+// let score_4="";
+// let score_5;
+// let score_6 = null;
+// let score_7 = undefined;
+
+
+// console.log(typeof(score_0))
+// console.log(typeof(score_0x))
+
+// let score_0xInNumber = Number(score_0x);
+// console.log(typeof(score_0xInNumber));  
+// console.log(score_0xInNumber);              //Got 33 which is correct
+
+// let score_1InNumber = Number(score_1);
+// console.log(score_1InNumber)                //Got here NaN(Not a number)
+
+// let score_2InNumber = Number(score_2);
+// console.log(typeof(score_2InNumber));
+// console.log(score_2InNumber);               //Got 1 which is correct
+
+// let score_3InNumber = Number(score_3);
+// console.log(typeof(score_3InNumber));
+// console.log(score_3InNumber);               //Got 0 which is correct
+
+// let score_4InNumber = Number(score_4);
+// console.log(typeof(score_4InNumber));
+// console.log(score_4InNumber);               //Got 0 when literal empty
+
+// let score_5InNumber = Number(score_5);
+// console.log(typeof(score_5InNumber));
+// console.log(score_5InNumber);               //Got Nan when literal only declares
+
+// let score_6InNumber = Number(score_6);
+// console.log(typeof(score_6InNumber));
+// console.log(score_6InNumber);               //Got 0 when literal assign with null
+
+// let score_7InNumber = Number(score_7);
+// console.log(typeof(score_7InNumber));
+// console.log(score_7InNumber);               //Got Nan when literal assign with undefined
+
+let isLogged=1;
+isNothing=""
+isSomething="Ramesh"
+
+let BooleanIsLogged = Boolean(isLogged);
+console.log(BooleanIsLogged)
+
+let BooleanIsNothing = Boolean(isNothing);
+console.log(BooleanIsNothing)
+
+let BooleanIsSomething = Boolean(isSomething);
+console.log(BooleanIsSomething)
+
+// ****BooleanConversion
+// If literal 1 then got true
+// If literal 0 then got false
+// If literal "" then got false
+// If literal "Ramesh" got true
